54303bae473979d41da73c7e42558c8917b088ac
HerrineKim <herrine.kim@gmail.com>
2022-01-24 23:22:06 +0900

    edit title

--- Python/실습파일/dust.py
+++
@@ -1,9 +0,0 @@
-dust = 100
-if 150 < dust:
-    print('매우나쁨')
-elif 80 <= dust <= 150:
-    print('나쁨')
-elif 31 <= dust <= 80:
-    print('보통')
-else:
-    print('좋음')
--- Python/실습파일/phone.py
+++
@@ -1,3 +0,0 @@
-phone_book = {'엄마': '1', '지혜': '2', '승운': '3'}
-print(phone_book)
-print(phone_book['지혜'])
