4b6a1609bad87461224bc9b49e53e4e0a5eed117
Hyerin Kim <aprilkim98@naver.com>
2022-07-17 11:00:07 +0900

    BOJ

--- Herrine's Algorithm/Problem Solving codes/BAEKJOON/9184_yay.py
+++ Herrine's Algorithm/Problem Solving codes/BAEKJOON/9184_yay.py
@@ -1,10 +1,9 @@
 # 9148. 신나는 함수 실행
-
 import sys
 input = sys.stdin.readline
 
 def w(a, b, c):
-    if a <= 0 or b<= 0 or c<=0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 1
     if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
@@ -16,10 +15,11 @@ def w(a, b, c):
     dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
     return dp[a][b][c]
 
-dp = [[[0]*(21) for _ in range(21)] for _ in range(21)]
+
+dp = [[[0] * (21) for _ in range(21)] for _ in range(21)]
 
 while 1:
     a, b, c = map(int, input().split())
-    if a ==-1 and b ==-1 and c ==-1:
+    if a == -1 and b == -1 and c == -1:
         break
     print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
