f3adfc2acd29b2add050e00d46950f5d1ad9cb20
hyerin <herrine.kim@gmail.com>
2023-01-10 14:16:42 +0900

    Update reducer.test.js

--- ETC/TDD/frontend-tdd-feconf2020/src/reducer.test.js
+++ ETC/TDD/frontend-tdd-feconf2020/src/reducer.test.js
@@ -5,8 +5,7 @@ import tasks from '../fixtures/tasks'
 describe('reducer', () => {
   describe('setTasks', () => {
     it('changes tasks array', () => {
-      const state = 
-      reducer({
+      const state = reducer({
         tasks: [],
       }, setTasks(tasks))
       expect(state.tasks).not.toHaveLength(0)
