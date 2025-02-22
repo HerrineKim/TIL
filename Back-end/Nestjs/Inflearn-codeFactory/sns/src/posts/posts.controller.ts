8434f5e798bfbbf21ab0c46d6107f02b9581177c
Hyerin <hkim@goingmerry.com>
2025-02-23 02:22:18 +0900

    add: ~ 7.1

--- Back-end/Nestjs/Inflearn-codeFactory/sns/src/posts/posts.controller.ts
+++ Back-end/Nestjs/Inflearn-codeFactory/sns/src/posts/posts.controller.ts
@@ -5,6 +5,8 @@ import {
   NotFoundException,
   Post,
   Body,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
@@ -17,7 +19,7 @@ interface PostModel {
   commentCount: number;
 }
 
-const posts: PostModel[] = [
+let posts: PostModel[] = [
   {
     id: 1,
     author: 'cillian_murphy',
@@ -45,6 +47,7 @@ const posts: PostModel[] = [
 ];
 @Controller('posts')
 export class PostsController {
+  // Nest.js가 자동으로 PostsService를 주입해줌.
   constructor(private readonly postsService: PostsService) {}
 
   // 1) GET /posts
@@ -86,9 +89,43 @@ export class PostsController {
     return post;
   }
 
-  // 4) PUT /posts/:id
+  // 4) PATCH /posts/:id
   //    특정 post를 수정한다.
+
+  @Patch(':id')
+  patchPost(
+    @Param('id') id: string,
+    @Param('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ): PostModel {
+    const post = posts.find((post) => post.id === parseInt(id));
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    return post;
+  }
 
   // 5) DELETE /posts/:id
   //    특정 post를 삭제한다.
+
+  @Delete(':id')
+  deletePost(@Param('id') id: string): string {
+    posts = posts.filter((post) => post.id !== parseInt(id));
+
+    return id;
+  }
 }
