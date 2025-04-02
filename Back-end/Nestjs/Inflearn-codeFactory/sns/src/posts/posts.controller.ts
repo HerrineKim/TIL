8fd2cdf8bcf8b71d930c376b24aced3ba9110d4b
Hyerin <hkim@goingmerry.com>
2025-04-02 23:44:46 +0900

    7-2: 모든 로직 service로 옮기기

--- Back-end/Nestjs/Inflearn-codeFactory/sns/src/posts/posts.controller.ts
+++ Back-end/Nestjs/Inflearn-codeFactory/sns/src/posts/posts.controller.ts
@@ -10,41 +10,6 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'cillian_murphy',
-    title: 'Dinner',
-    content: 'I had a great dinner at the restaurant last night.',
-    likeCount: 100000,
-    commentCount: 9999,
-  },
-  {
-    id: 2,
-    author: 'cillian_murphy',
-    title: 'filming',
-    content: 'I am filming a new movie.',
-    likeCount: 100000,
-    commentCount: 9999,
-  },
-  {
-    id: 3,
-    author: 'cillian_murphy',
-    title: 'Peaky Blinders',
-    content: 'Yay!',
-    likeCount: 100000,
-    commentCount: 9999,
-  },
-];
 @Controller('posts')
 export class PostsController {
   // Nest.js가 자동으로 PostsService를 주입해줌.
@@ -53,19 +18,15 @@ export class PostsController {
   // 1) GET /posts
   //    모든 posts를 가져온다.
   @Get()
-  getPosts(): PostModel[] {
-    return posts;
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
 
   // 2) GET /posts/:id
   //    특정 post를 가져온다.
   @Get(':id')
-  getPost(@Param('id') id: string): PostModel {
-    const post = posts.find((post) => post.id === parseInt(id));
-    if (!post) {
-      throw new NotFoundException('Post not found');
-    }
-    return post;
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts
@@ -76,17 +37,8 @@ export class PostsController {
     @Body('author') author: string,
     @Body('title') title: string,
     @Body('content') content: string,
-  ): PostModel {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-    posts.push(post);
-    return post;
+  ) {
+    return this.postsService.createPost(author, title, content);
   }
 
   // 4) PATCH /posts/:id
@@ -98,34 +50,15 @@ export class PostsController {
     @Param('author') author: string,
     @Body('title') title: string,
     @Body('content') content: string,
-  ): PostModel {
-    const post = posts.find((post) => post.id === parseInt(id));
-    if (!post) {
-      throw new NotFoundException('Post not found');
-    }
-
-    if (author) {
-      post.author = author;
-    }
-
-    if (title) {
-      post.title = title;
-    }
-
-    if (content) {
-      post.content = content;
-    }
-
-    return post;
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   // 5) DELETE /posts/:id
   //    특정 post를 삭제한다.
 
   @Delete(':id')
-  deletePost(@Param('id') id: string): string {
-    posts = posts.filter((post) => post.id !== parseInt(id));
-
-    return id;
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
   }
 }
--- Back-end/Nestjs/Inflearn-codeFactory/sns/src/posts/posts.service.ts
+++ Back-end/Nestjs/Inflearn-codeFactory/sns/src/posts/posts.service.ts
@@ -4,7 +4,101 @@
 // 데이터베이스와 연결되는 로직은 서비스에 작성
 
 // Nest.js에서는 컨트롤러를 복잡하게 만들지 않고, 서비스에 비즈니스 로직을 작성하여 컨트롤러에서 호출하는 방식을 사용하라고 권장함.
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'cillian_murphy',
+    title: 'Dinner',
+    content: 'I had a great dinner at the restaurant last night.',
+    likeCount: 100000,
+    commentCount: 9999,
+  },
+  {
+    id: 2,
+    author: 'cillian_murphy',
+    title: 'filming',
+    content: 'I am filming a new movie.',
+    likeCount: 100000,
+    commentCount: 9999,
+  },
+  {
+    id: 3,
+    author: 'cillian_murphy',
+    title: 'Peaky Blinders',
+    content: 'Yay!',
+    likeCount: 100000,
+    commentCount: 9999,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts.push(post);
+    return post;
+  }
+
+  updatePost(postId: number, author: string, title: string, content: string) {
+    const post = posts.find((post) => post.id === postId);
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    return post;
+  }
+
+  deletePost(postId: number) {
+    const post = posts.find((post) => post.id === postId);
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+    posts.splice(
+      posts.findIndex((post) => post.id === postId),
+      1,
+    );
+    return postId;
+  }
+}
